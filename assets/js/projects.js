de6ffa0f9e742f4adfa5e02f03a61b7758adb368
Ashish Kumar <ashish@ashishku.in>
2023-01-21 08:33:23 +0530

    projects

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -3,11 +3,15 @@ function selectCards(type) {
   const marginVertical = 10;
   const projectsContainer = document.querySelector(".projects");
   const cardsContainer = projectsContainer.querySelector(".projects__card");
-  const cards = Array.from(cardsContainer.querySelectorAll(".project"));
+  const cards = Array.from(cardsContainer.querySelectorAll(".card"));
   const containerRect = cardsContainer.getBoundingClientRect();
-  const firstCardRect = cards[0].getBoundingClientRect();
-  const cardWidth = firstCardRect.width + marginHorizontal;
-  const cardsPerRow = Math.floor(containerRect.width / cardWidth);
+  const cardWidths = cards.map((card) => {
+    const cardRect = card.getBoundingClientRect();
+    return cardRect.width;
+  });
+  const maxCardWidth = Math.max(...cardWidths);
+  const cardWidth = maxCardWidth + 2 * marginHorizontal;
+  const cardsPerRow = Math.floor(containerRect.width / cardWidth) || 1;
   const containerMargin =
     (containerRect.width - cardsPerRow * cardWidth + marginHorizontal) / 2;
   const colHeights = Array(cardsPerRow).fill(marginVertical);
@@ -24,6 +28,7 @@ function selectCards(type) {
         colHeights[minIndex] + cardRect.height + marginVertical;
 
       card.style.transform = `scale(1) translate3d(${left}px, ${top}px, 0px)`;
+      card.style.width = `${maxCardWidth}px`;
       card.style.opacity = 1;
     } else {
       card.style.opacity = 0;
@@ -31,12 +36,13 @@ function selectCards(type) {
   });
 
   const maxHeight = Math.max(...colHeights);
-  projectsContainer.style.height =
+  projectsContainer.style["min-height"] =
     maxHeight + containerRect.y + marginVertical + "px";
+  projectsContainer.style.transform = `scale(1)`;
+  projectsContainer.style.transition = "min-height .5s ease";
 }
 
 function onLoad() {
-  selectCards("all");
   const selectors = Array.from(
     document.querySelectorAll(".projects__selector span")
   );
@@ -52,6 +58,18 @@ function onLoad() {
       selectCards(selector.dataset.type);
     });
   });
+
+  selectCards("all");
+  const selectAll = document.querySelector(
+    ".projects__selector span[data-type='all']"
+  );
+  selectAll.classList.add("select");
+}
+
+function onResize() {
+  const selected = document.querySelector(".projects__selector span.select");
+  selectCards(selected.dataset.type);
 }
 
 window.addEventListener("load", onLoad);
+window.addEventListener("resize", onResize);
